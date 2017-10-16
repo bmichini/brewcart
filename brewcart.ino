3be02fb9c84e81c07eb6d3e6367e38a4574a0d03
bmichini <bmichini@airware.com>
2017-10-16 13:23:40 -0700

    Fixed broken merge

--- brewcart.ino
+++ brewcart.ino
@@ -50,6 +50,7 @@ float kettle_watts = 0.0;
 int kettle_dutycycle_ms = 0;
 void setKettleWatts ( float watts )
 {
+  kettle_watts = constrain(watts, 0.f, KETTLE_MAX_WATTS);
   kettle_dutycycle_ms = (int) ( kettle_watts / KETTLE_TOTAL_WATTS * PWM_PERIOD_MS );
 }
 
@@ -62,6 +63,7 @@ float rims_watts = 0.0;
 int rims_dutycycle_ms = 0;
 void setRIMSWatts ( float watts )
 {
+  rims_watts = constrain(watts, 0.f, RIMS_MAX_WATTS);
   rims_dutycycle_ms = (int) ( rims_watts / RIMS_TOTAL_WATTS * PWM_PERIOD_MS );
 }
 
@@ -75,6 +77,7 @@ const float RIMS_SETPOINT_MAX_F = 185.0;
 float rims_setpoint_f = RIMS_SETPOINT_MIN_F;
 void setRIMStempF( float tempF )
 {
+  rims_setpoint_f = constrain(tempF, RIMS_SETPOINT_MIN_F, RIMS_SETPOINT_MAX_F);
 }
 
 // PWM CONTROL
@@ -262,19 +265,6 @@ void loop() {
     setRIMSWatts( 0 );
   }
 
-<<<<<<< HEAD
-=======
-  // Calculate delta in wattage necessary to get RIMS temp to set point
-  // Calculated based on flow rate of water and temperature delta
-  float dwatts = (degF2degC(rims_setpoint_f) - degF2degC(tempF_RIMS))*flowrate_gpm / LPS_2_GPM * 4184.0;
-
-  // Rate limit the delta wattage to prevent chatter
-  dwatts = constrain(dwatts, RIMS_MIN_DWATTS, RIMS_MAX_DWATTS);
-
-  // Set RIMS wattage (set function applies limits)
-  setRIMSWatts( rims_watts + dwatts );
-
->>>>>>> fb0833d61384c16c24748783edcafbd8f74333ac
   // Update the LCD display
   lcdCount++;
   if (lcdCount % 4 == 0){updateLCD();}
